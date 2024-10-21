f7f43f342cf7b52b1b218c4397f6a34c869bd123
CarlosNegrao <cnegrao2@gmail.com>
2024-10-20 22:48:19 -0300

    prd 3 rasc

--- utils_files.py
+++ utils_files.py
@@ -15,14 +15,17 @@ CACHE_DESCONVERTE = {}
 
 def converte_nome_mensagem(nome_mensagem):
     nome_arquivo = unidecode(nome_mensagem)
-    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    nome_arquivo = re.sub(r'\W+', '', nome_arquivo).lower()
     return nome_arquivo
+
 
 def desconverte_nome_mensagem(nome_arquivo):
     if not nome_arquivo in CACHE_DESCONVERTE:
-        nome_mensagem = ler_mensagem_por_nome_arquivo(nome_arquivo, key='nome_mensagem')
+        nome_mensagem = ler_mensagem_por_nome_arquivo(
+            nome_arquivo, key='nome_mensagem')
         CACHE_DESCONVERTE[nome_arquivo] = nome_mensagem
     return CACHE_DESCONVERTE[nome_arquivo]
+
 
 def retorna_nome_da_mensagem(mensagens):
     nome_mensagem = ''
@@ -31,6 +34,7 @@ def retorna_nome_da_mensagem(mensagens):
             nome_mensagem = mensagem['content'][:30]
             break
     return nome_mensagem
+
 
 def salvar_mensagens(mensagens):
     if len(mensagens) == 0:
@@ -43,10 +47,12 @@ def salvar_mensagens(mensagens):
     with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
         pickle.dump(arquivo_salvar, f)
 
+
 def ler_mensagem_por_nome_arquivo(nome_arquivo, key='mensagem'):
     with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
         mensagens = pickle.load(f)
     return mensagens[key]
+
 
 def ler_mensagens(mensagens, key='mensagem'):
     if len(mensagens) == 0:
@@ -57,16 +63,20 @@ def ler_mensagens(mensagens, key='mensagem'):
         mensagens = pickle.load(f)
     return mensagens[key]
 
+
 def listar_conversas():
     conversas = list(PASTA_MENSAGENS.glob('*'))
-    conversas = sorted(conversas, key=lambda item: item.stat().st_mtime_ns, reverse=True)
+    conversas = sorted(
+        conversas, key=lambda item: item.stat().st_mtime_ns, reverse=True)
     return [c.stem for c in conversas]
 
 # SALVAMENTO E LEITURA DA APIKEY ========================
 
+
 def salva_chave(chave):
     with open(PASTA_CONFIGERACOES / 'chave', 'wb') as f:
         pickle.dump(chave, f)
+
 
 def le_chave():
     if (PASTA_CONFIGERACOES / 'chave').exists():
